4df5379a39a18f7115a30a6e18d4edbcb634df33
abdulhafizk <hafizh.abdulhafizhkustiawan@gmail.com>
2024-06-03 20:37:13 +0700

    Add js File (#2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import colors from "tailwindcss/colors";
+import type { Config } from "tailwindcss";
 
+const config: Config = {
+  content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+      },
+      colors: {
+        accent: {
+          main: colors.pink[400],
+          start: colors.purple[500],
+          end: colors.pink[400],
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
+
+export default config;
